b611bc66653c85ea7c44bef0154adcda61f92949
MrMartinPet <martin_pet@hotmail.com>
2026-07-19 14:43:37 +0200

    Refresh mobile app cache

--- sw.js
+++ sw.js
@@ -1,4 +1,20 @@
-const CACHE="transcriber-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="transcriber-v2";
+const ASSETS=["./manifest.webmanifest"];
+self.addEventListener("install",event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
+    self.clients.claim()
+  ]));
+});
+self.addEventListener("fetch",event=>{
+  const req=event.request;
+  if(req.mode==="navigate"){
+    event.respondWith(fetch(req).catch(()=>caches.match("./index.html")));
+    return;
+  }
+  event.respondWith(caches.match(req).then(hit=>hit||fetch(req)));
+});
